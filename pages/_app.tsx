dcea52d96527d210c36a08ed03fb8c319309f73c
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2022-06-12 01:37:58 +0900

    Recoilとrecoil-persistをインストールし、ログインユーザーの情報を保持するストアを作成 #2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import '../styles/globals.css'
 import type{ NextPage } from 'next'
 import type { AppProps } from 'next/app'
+import { RecoilRoot } from 'recoil';
 
 const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => {
   return (
-    <Component {...pageProps} />
+    <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
   );
 };
 
-export default MyApp
+export default MyApp;
