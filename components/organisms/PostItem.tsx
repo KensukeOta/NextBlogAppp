b655cb0a5dbe24d605475e39af983e98c9c38ef1
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2022-06-12 23:24:17 +0900

    記事の内容を表示するページと、そのページに遷移するリンクを実装 #8

--- components/organisms/PostItem.tsx
+++ components/organisms/PostItem.tsx
@@ -1,10 +1,13 @@
+import Link from "next/link";
 import type { FC } from "react";
 import type { PostProps } from "../../types/PostProps";
 
 export const PostItem: FC<PostProps> = (props) => {
   return (
     <li className="border">
-      <h1 className="font-bold">{props.post!.title}</h1>
+      <Link href={`/posts/${props.post!.id}`}>
+        <a><h1 className="font-bold">{props.post!.title}</h1></a>
+      </Link>
       <nav>
         <p>by {props.post!.user.name}</p>
       </nav>
---
+++ pages/posts/[id].tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import { Layout } from "../../components/templates/Layout";
+
+const PostContent: NextPage = () => {
+  return (
+    <Layout>
+      <Head>
+        <title></title>
+      </Head>
+    </Layout>
+  );
+};
+
+export default PostContent;
