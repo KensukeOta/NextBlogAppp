98b5e99b82c646a02090a9999cbf17c4dbb74d9c
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2022-06-12 14:37:45 +0900

    ユーザー登録フォームに必須タグを付与

--- components/atoms/Label.tsx
+++ components/atoms/Label.tsx
@@ -1,8 +1,12 @@
 import type { FC } from "react";
 import type { Props } from "../../types/Props";
+import { useRouter } from "next/router";
+import { RequiredText } from "./RequiredText";
 
 export const Label: FC<Props> = (props) => {
+  const router = useRouter();
+  
   return (
-    <label htmlFor={props.for}>{props.children}</label>
+    <label htmlFor={props.for}>{props.children} {router.pathname === "/signup" && <RequiredText />}</label>
   );
 };
---
+++ components/atoms/RequiredText.tsx
@@ -0,0 +1,7 @@
+import type { FC } from "react";
+
+export const RequiredText: FC = () => {
+  return (
+    <span className="text-red-500">*必須</span>
+  );
+};
